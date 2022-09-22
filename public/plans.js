0f9ace15d63627a038f83516e7f6cc3b8ad20250
Trayshawn <twebb@recurly.com>
2022-09-22 12:21:41 -0600

    changed button to checkout and fixed formatting

--- public/plans.js
+++ public/plans.js
@@ -2,11 +2,11 @@ const { plans, publicKey } = window.recurlyConfig;
 
 const radioPlans = document.querySelector('.js-plan-select');
 
-const planOptions2 = plans.reduce((options, plan) => {
+const planOptions = plans.reduce((options, plan) => {
 	options += `<div> <input type="radio" id="${plan.code}" name="plan" value=${plan.code}>
   <label for="${plan.code}">${plan.name} - $${plan.unit_amount}</label>
   </div>`;
 	return options;
 }, "");
 
-radioPlans.innerHTML = planOptions2;
+radioPlans.innerHTML = planOptions;
